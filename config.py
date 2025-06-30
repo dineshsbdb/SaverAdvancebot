4b69352ec85c3cb9537f21988f0c46887183d6a4
Anonymous <dinc9433@gmail.com>
2025-06-30 13:35:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7943633200:AAGp-vAfdX3vhcZ71T2iOqUTloMaSogjjTo"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8171835867").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jihehod332:OM69Q4epgIEcN3xk@cluster0.qzw02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002558064392")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002558064392"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002589507150"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "1"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
