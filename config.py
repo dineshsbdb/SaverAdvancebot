49743ad83c0122377caebac932d2e6bb2f1f68ea
India <122437237+dineshsbdb@users.noreply.github.com>
2026-01-24 22:16:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8073469304:AAFQHcqe8XA2f9IxV0y-phGQIAwA2trnbGg"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8171835867").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jihehod332:OM69Q4epgIEcN3xk@cluster0.qzw02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1003056575939")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003056575939"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002589507150"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
